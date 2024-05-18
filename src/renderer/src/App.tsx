5165ed1aaa7ba7aef5b98148a0ad5ee8a2d47ac1
zhubangrui <zhubangrui@duoblock.cn>
2024-05-18 14:59:45 +0800

    init

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <div className=" bg-slate-500 text-2xl p-3">Hello!</div>
+    </>
+  )
+}
+
+export default App
